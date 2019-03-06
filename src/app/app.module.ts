33372aebf466331257d80216a1b254dae08fd8a0
omarfarid <omarfarid778@gmail.com>
2019-03-06 12:57:54 +0600

    helth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+
+  ],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
